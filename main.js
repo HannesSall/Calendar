906a115e0ad2d7dd425202fb183827e14609795a
mjauwoff <98marfra@skola.boras.se>
2026-04-30 14:10:30 +0200

    added crocodiles arrows and fixed updating calendar and dates

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
+let month = new Date().toLocaleDateString("sv-SE", { month: "numeric" }) - 1;
+let year = new Date().toLocaleDateString("sv-SE", { year : "numeric"});
+
 document.addEventListener("DOMContentLoaded", main)
 
-
 function main() {
-    let month = new Date().toLocaleDateString("sv-SE", { month: "numeric" });
-    SetMonth();
-    renderCalendar(2026, month-1)
-    GetDate();
+    SetMonth(year, month);
+    SetupListeners();
+    renderCalendar()
+    GetDate(year, month);
 }
 
-function renderCalendar(year, month) {
+function renderCalendar() {
     const grid = document.querySelector(".calendar-grid");
 
     // Clear previous content
     grid.innerHTML = "";
+
 
     const firstDay = new Date(year, month, 1).getDay();
 
@@ -37,16 +40,48 @@ function renderCalendar(year, month) {
     }
 }
 
-function GetDate()
-{
-    let year = new Date().toLocaleDateString("sv-SE", {year: "numeric"});
-    let month = new Date().toLocaleDateString("sv-SE", {month: "2-digit"});
-    let day = new Date().toLocaleDateString("sv-SE", {day: "2-digit"});
+function GetDate(year, month) {
+    let day = new Date().toLocaleDateString("sv-SE", { day: "2-digit" });
     document.querySelector(".date-and-time-section").textContent = year + "-" + month + "-" + day;
 }
 
-function SetMonth() {
-    let month = new Date().toLocaleDateString("sv-SE", { month: "long"});
-    let year = new Date().toLocaleDateString("sv-SE", {year: "numeric"});
-    document.querySelector(".month").textContent = month + " " + year;
+function SetMonth(year, month) {
+    let monthStr = new Date(year, month).toLocaleDateString("sv-SE", { 
+        month: "long" 
+    });
+
+    document.querySelector(".month").textContent = monthStr + " " + year;
+}
+
+function SetupListeners() {
+    document.addEventListener("click", (event) => {
+        if (event.target.matches(".left-btn-month")) {
+            month--;
+
+            if (month < 0) {
+                month = 11;
+                year--;
+            }
+
+            renderCalendar();
+            SetMonth(year, month);
+            GetDate(year, month);
+        }
+
+    });
+    document.addEventListener("click", (event) => {
+        if (event.target.matches(".right-btn-month")) {
+            month++;
+            
+            if (month > 11) {
+                month = 0;
+                year++;
+            }
+
+            renderCalendar();
+            SetMonth(year, month);
+            GetDate(year, month);
+        }
+
+    });
 }
