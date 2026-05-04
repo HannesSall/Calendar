aaa38b2eff6ad04670704cfbdfe0b3b78f8d7fc0
HannesSall <hannes.sallstedt@gmail.com>
2026-05-04 15:38:52 +0200

    Fix style for todo

---
+++ todo.js
@@ -0,0 +1,50 @@
+function renderTodos() {
+  const todoListElement = document.getElementById("todoList");
+  todoListElement.innerHTML = "";
+
+  for (let index = 0; index < todos.length; index++) {
+    const todo = todos[index];
+
+    const card = createTodoCard(todo);
+
+    todoListElement.append(card);
+    todoListElement.className = "todoListClass";
+  }
+}
+
+function createTodoCard(todo) {
+  const article = document.createElement("article");
+  article.className = "card";
+
+  const title = document.createElement("h2");
+  title.textContent = "Titel: " + todo.title;
+  title.className = "card-title";
+
+  const desc = document.createElement("p");
+  desc.textContent = "Beskrivning: " + todo.description;
+  desc.className = "card-description";
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = todo.completed;
+
+  const label = document.createElement("label");
+  label.append("Avklarad: ", checkbox);
+
+  const completed = document.createElement("p");
+  completed.textContent = 
+    "Skapad: " + new Date(todo.createdAt).toLocaleDateString("sv-SE");
+  completed.className = "card-created";
+
+  const due = document.createElement("p");
+  due.textContent =
+    "Förfaller: " +
+    (todo.dueDate
+      ? new Date(todo.dueDate).toLocaleDateString("sv-SE")
+      : "Ingen deadline");
+  due.className = "card-due";
+
+  article.append(title, desc, label, completed, due);
+
+  return article;
+}
